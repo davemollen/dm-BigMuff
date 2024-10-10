fbf7ec693a08bb136ca9d2669677fe0f0d6e2cb3
Dave Mollen <davemollen@gmail.com>
2024-10-10 19:50:02 +0200

    Update dm-big-muff.mk

--- .github/workflows/dm-big-muff.mk
+++ .github/workflows/dm-big-muff.mk
@@ -9,17 +9,14 @@ DM_BIG_MUFF_SITE = https://github.com/davemollen/dm-BigMuff.git
 DM_BIG_MUFF_SITE_METHOD = git
 DM_BIG_MUFF_BUNDLES = dm-BigMuff.lv2
 
-# Nightly toolchain is needed to enable simd
-define DM_BIG_MUFF_CONFIGURE_CMDS
-	~/.cargo/bin/rustup toolchain install nightly-2024-08-07
-	~/.cargo/bin/rustup default nightly-2024-08-07
-	~/.cargo/bin/rustup target add $(MOD_PLUGIN_BUILDER_RUST_TARGET)
-endef
-
 define DM_BIG_MUFF_BUILD_CMDS
+	~/.cargo/bin/rustup default nightly
+
 	rm -f $(@D)/lv2/dm-BigMuff.lv2/libdm_big_muff.so
 	(cd $(@D)/lv2 && \
 		~/.cargo/bin/cargo build $(MOD_PLUGIN_BUILDER_RUST_BUILD_FLAGS))
+
+	~/.cargo/bin/rustup default stable
 endef
 
 define DM_BIG_MUFF_INSTALL_TARGET_CMDS
