4abb84226a4d30c13a2144a9fb5455b6a7c3f3d4
davemollen <davemollen@gmail.com>
2024-12-23 18:42:13 +0100

    Update smoothing implementation

--- profiling/benches/big_muff_bench.rs
+++ profiling/benches/big_muff_bench.rs
@@ -1,17 +1,19 @@
 #[path = "../src/utils.rs"]
 mod utils;
-use big_muff::BigMuff;
+use big_muff::{BigMuff, Params};
 use criterion::{criterion_group, criterion_main, Criterion};
 use utils::generate_signal_stream;
 
 fn big_muff_bench(c: &mut Criterion) {
   let mut big_muff = BigMuff::new(44100.);
+  let mut params = Params::new(44100.);
+  params.set(0.5, 0.5, 0.5);
   let signal_stream = generate_signal_stream(44100);
 
   c.bench_function("big_muff", |b| {
     b.iter(|| {
       for signal in &signal_stream {
-        big_muff.process(*signal, 0.5, 0.5, 0.5);
+        big_muff.process(*signal, &mut params);
       }
     })
   });
--- profiling/src/main.rs
+++ profiling/src/main.rs
@@ -1,12 +1,14 @@
 mod utils;
-use big_muff::BigMuff;
+use big_muff::{BigMuff, Params};
 use utils::generate_signal;
 
 fn main() {
   let mut big_muff = BigMuff::new(44100.);
+  let mut params = Params::new(44100.);
+  params.set(0.5, 0.5, 0.5);
 
   loop {
     let input = generate_signal();
-    big_muff.process(input, 0.5, 0.5, 0.5);
+    big_muff.process(input, &mut params);
   }
 }
